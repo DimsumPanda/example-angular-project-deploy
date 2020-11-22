c11786705648983b625a4894e86ec924afcd2254
DimsumPanda <dimsumpanda>
2020-11-22 13:02:05 -0500

    add 22 port

--- main.tf
+++ main.tf
@@ -18,6 +18,18 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_launch_configuration" "example" {
